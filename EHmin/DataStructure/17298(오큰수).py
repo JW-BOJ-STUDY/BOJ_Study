8c0d07f515a1bfb7e7fd59243065d5d5af590249
taemin-steve <okchrisok@naver.com>
2023-01-17 16:54:41 +0900

    17298(fail)

---
+++ EHmin/DataStructure/17298(오큰수).py
@@ -0,0 +1,12 @@
+n = int(input())
+A = list(map(int, input().split()))
+answer = [-1] * n
+stack = []
+
+stack.append(0)
+for i in range(1, n):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
+
+print(*answer)
