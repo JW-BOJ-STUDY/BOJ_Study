0384d3d23d923f1f79f2faf77eb3fcf5f2b6d6c6
taemin-steve <okchrisok@naver.com>
2023-01-25 11:09:29 +0900

    11656

--- EHmin/DataStructure/11656(접미사배열).py
+++ EHmin/DataStructure/11656(접미사배열).py
@@ -1,1 +1,13 @@
-int 
+from sys import stdin
+input = stdin.readline
+
+arr = input().strip()
+suffix = []
+
+for i in range(len(arr)):
+    suffix.append(arr[i:])
+suffix.sort()
+
+for i in range(len(arr)):
+    print(suffix[i])
+    
