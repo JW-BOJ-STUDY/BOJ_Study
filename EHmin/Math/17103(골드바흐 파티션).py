5f10e59dff2221c59f7c7d29b8f1f26944681b32
taemin-steve <okchrisok@naver.com>
2023-02-03 20:31:01 +0900

    fail17103

--- EHmin/Math/17103(골드바흐 파티션).py
+++ EHmin/Math/17103(골드바흐 파티션).py
@@ -5,7 +5,7 @@ input = stdin.readline
 print = stdout.write
 
 def is_prime_number(a):
-    if a == 1: # 1에대한 예외 처리를 잊지 말고 해주어야 한다. 
+    if a == 1: # 1에대한 예외 처리를 잊지 말고 해주어야 한다
         return False
     for i in range(2,int(math.sqrt(a))+1): #루트 N 이상의 경우는 확인할 필요가 없다. 
         if (a % i) == 0:
